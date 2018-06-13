51b5bb47eed18742d34cc02e205559b09fe0563a
Zhusheng <zhusheng@megvii.com>
2018-06-13 17:08:49 +0800

    bugfix

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"sync/atomic"
 
@@ -17,31 +15,39 @@ import (
 )
 
 var (
-	flagRequest     = flag.Uint("n", 1, "Request")
+	flagRequest     = flag.Uint("n", 1, "Total request")
 	flagConcurrency = flag.Uint("c", 1, "Concurrency")
-	flagOutput      = flag.String("out", "", "Output file")
-
-	newLine = []byte("\n")
+	flagOutput      = flag.String("o", "-", "Output file, '-':stdout, '':null, 'filepath':'filepath-<id>.out'")
 )
 
-type nopCloser struct {
-	io.Writer
+func logf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", v...)
 }
 
-func (nopCloser) Close() error { return nil }
+type discard struct{}
+
+func (d discard) Close() error                      { return nil }
+func (d discard) Write(p []byte) (n int, err error) { return len(p), nil }
+
+type stdout struct{}
+
+func (s stdout) Close() error { return nil }
+func (s stdout) Write(p []byte) (n int, err error) {
+	return os.Stdout.Write(p)
+}
 
 func openOutput(id int) (io.WriteCloser, error) {
 	switch *flagOutput {
 	case "":
-		return nopCloser{ioutil.Discard}, nil
+		return discard{}, nil
 	case "-":
-		return nopCloser{os.Stdout}, nil
+		return stdout{}, nil
 	default:
-		return os.Create(fmt.Sprintf("%s.%d", *flagOutput, id))
+		return os.Create(fmt.Sprintf("%s-%d.out", *flagOutput, id))
 	}
 }
 
-func worker(ctx context.Context, u *url.URL, id int) error {
+func worker(u *url.URL, id int) error {
 	output, err := openOutput(id)
 	if err != nil {
 		return err
@@ -56,44 +62,32 @@ func worker(ctx context.Context, u *url.URL, id int) error {
 	defer conn.Close()
 
 	for {
-		msgType, r, err := conn.NextReader()
+		msgType, content, err := conn.ReadMessage()
 		if err != nil {
 			return err
 		}
 		switch msgType {
-		case websocket.TextMessage:
-		case websocket.BinaryMessage:
-			io.Copy(output, r)
-			output.Write(newLine)
+		case websocket.TextMessage, websocket.BinaryMessage:
+			output.Write(content)
 		}
 	}
 }
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [opts] connect \nopts:\n", os.Args[0])
+		const usage = `Usage: wsbm [options] <url>
+    '<id>' in url will be replace by connection id
+options:`
+		fmt.Fprintln(os.Stderr, usage)
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	u, err := url.Parse(flag.Arg(0))
-	if err != nil {
-		panic(err)
-	}
-
-	switch u.Scheme {
-	case "http":
-		u.Scheme = "ws"
-	case "https":
-		u.Scheme = "wss"
-	default:
-	}
-
 	req, con := int(*flagRequest), int(*flagConcurrency)
 	if con > req {
-		con = req
+		req = con
 	}
-	log.Println("n:", req, "c:", con)
+	logf("request: %d, concurrency:%d", req, con)
 
 	var wg sync.WaitGroup
 	wg.Add(con)
@@ -108,9 +102,22 @@ func main() {
 					return
 				}
 
-				log.Println("+", id, u.String())
-				if err = worker(nil, u, id); err != nil {
-					log.Println("-", id, err)
+				u, err := url.Parse(strings.Replace(flag.Arg(0), "<id>", fmt.Sprint(id), -1))
+				if err != nil {
+					panic(err)
+				}
+
+				switch u.Scheme {
+				case "http":
+					u.Scheme = "ws"
+				case "https":
+					u.Scheme = "wss"
+				default:
+				}
+
+				logf("+ %d %s", id, u.String())
+				if err = worker(u, id); err != nil {
+					logf("- %d %s", id, err)
 				}
 			}
 		}()
